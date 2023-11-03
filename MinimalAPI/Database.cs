bb4d01732472476b5282078e46f0e2777f1e4dbd
alexsandev <alex.rsjr10@gmail.com>
2023-11-02 21:07:42 -0300

    Web API, swagger and MySQL connection (using Dapper) working

---
+++ MinimalAPI/Database.cs
@@ -0,0 +1,21 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace MinimalAPI
+{
+    public class Database
+    {
+        private static MySqlConnection? _connection;
+
+        public static MySqlConnection GetConnection()
+        {
+            if (_connection == null)
+            {
+                _connection = new MySqlConnection("Server=localhost;User ID=root;Password=Ar@1998!#;Database=testedb");
+                _connection.Open();
+            }
+            return _connection;
+        }
+
+    }
+}
+
--- MinimalAPI/Program.cs
+++ MinimalAPI/Program.cs
@@ -1,13 +1,53 @@
+using Dapper.Contrib.Extensions;
+
 namespace MinimalAPI
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            var connection = Database.GetConnection();
+
             var builder = WebApplication.CreateBuilder(args);
+
+            //Habilitando o swagger
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            app.MapGet("/", () => "API inicializada");
+
+            app.MapGet("/users", () => {
+                return connection.GetAll<User>().ToList();
+            });
+
+            app.MapGet("/users/{id}", (int id) => {
+                return connection.Get<User>(id);
+            });
+
+            app.MapPost("/users/create", (User user) => {
+                connection.Insert(user);
+            });
+
+            app.MapPut("/users/update/{id}", (int id, User obj) => {
+                User user = connection.Get<User>(id);
+                if(user != null)
+                {
+                    user.Name = obj.Name;
+                    user.Email = obj.Email;
+                    user.Password = obj.Password;
+                    connection.Update(user);
+                }
+            });
+
+            app.MapDelete("/users/delete/{id}", (int id) => {
+                connection.Delete(new User(id, null, null, null));
+            });
+
+            app.UseSwagger();// Ativando o Swagger
+
+            app.UseSwaggerUI();// Ativando a interface Swagger
 
             app.Run();
         }
